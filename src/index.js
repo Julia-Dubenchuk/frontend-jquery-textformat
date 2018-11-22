7c734a27378bac9be9b8d6a672056e05b36ccf83
dubenchuk6@gmail.com <dubenchuk6@gmail.com>
2018-11-22 20:07:25 +0200

    add regexp

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './styles/style.css';
 import $ from 'jquery';
+
+let text = $('.message-field');
+let output = $('.message-formatted');
+let regexp = //igm;
+output[0].innerHTML = 'dsfsdfsf';
+text.change(function() {
+	output[0].innerHTML = text[0].value.replace(/ {2,}/igm,' ').replace(/^ [^\s]{1,}/,'').toLowerCase();
+});
