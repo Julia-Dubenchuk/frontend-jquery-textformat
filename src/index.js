e42dce3e58ab361f44da87700d2ba9db06784cde
Yulia Dubenchuk <dubenchuk6@gmail.com>
2018-11-27 01:20:16 +0200

    add variable and function for formatted message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import './styles/style.css';
 import $ from 'jquery';
 
-let getText = $('.message-field');
+let fieldMessage = $('.message-field');
+let outputMessage = $('.message-formatted');
 
-getText.bind('input', function () {
-	$('.message-formatted')[0].innerHTML = getText[0].value.replace(/\s{2,}|\n/igm, ' ').replace(/^\s+/, '').toLowerCase();
+function formattedMessage () {
+	let searchSpaceAndWrap = /\s{2,}|\n/igm;
+
+	return fieldMessage[0].value.trim().replace(searchSpaceAndWrap, ' ').toLowerCase();
+}
+
+fieldMessage.on('input', function () {
+	outputMessage.text(formattedMessage());
 });
